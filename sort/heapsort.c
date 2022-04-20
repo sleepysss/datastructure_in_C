2d46ad71aef6f8945a124f797e9776951a64eb5e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-04-20 19:45:56 +0800

    Update heapsort.c

--- sort/heapsort.c
+++ sort/heapsort.c
@@ -4,14 +4,16 @@
 
 //maxheap: result will be small to big
 
-void reheapDown(int store[],int root,int last)
-{
+void reheapDown(int store[],int root,int last) //last:last index
+{	
+	//找root,left_child,right_child三者中最大的且子節點要在有效範圍內
 	int biggestindex = root;
 	if (2 * root + 1 <= last && store[2 * root + 1] > store[biggestindex])
 		biggestindex = 2 * root + 1;
 	if (2 * root + 2 <= last && store[2 * root + 2] > store[biggestindex])
 		biggestindex = 2 * root + 2;
-	if (biggestindex != root)
+	//若有調整,則檢查新的subtree是否符合
+	if (biggestindex != root) 
 	{
 		int temp = store[root];
 		store[root] = store[biggestindex];
@@ -22,6 +24,7 @@ void reheapDown(int store[],int root,int last)
 
 void BuildHeapFromArray(int store[],int last)
 {
+	//對所有「具有child的node」檢查一次，便能夠把一個任意矩陣調整成Max Heap
 	for (int i = (last - 1) / 2; i >= 0; --i)
 		reheapDown(store, i, last);
 }
@@ -32,11 +35,11 @@ void heapsort(int store[],int last)
 
 	for (int i = last; i > 0; --i)
 	{
-		int temp = store[0];       //(1)把最大的移到最後面(由後往前一路加回來(靠last)) (2)整個array分為sort完和沒sort完
-		store[0] = store[last];
+		int temp = store[0];         //把最前面的(最大)移到最後面,最後面的移到最前面
+		store[0] = store[last];  
 		store[last] = temp;
-		last--;
-		reheapDown(store, 0, last);
+		last--;                      //把array分為sor完和梅sort完兩部分
+		reheapDown(store, 0, last);  //調整最前面的到適當位置(可以這樣的原因為其他都是符合heap特性的)
 	}
 }
 
