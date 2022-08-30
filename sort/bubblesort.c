5662efde51b40aae1148f5337c9af7ec3a049e6b
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-08-30 15:42:43 +0800

    Update bubblesort.c

--- sort/bubblesort.c
+++ sort/bubblesort.c
@@ -1,53 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 10
 
 int main()
 {
-	//bubble sort:讓數字大(小)的慢慢往右移動
+    int temp,flag=1,size=10;
+    int store[10]={5,7,6,3,8,9,1,2,5,4};
+    
+    for(int i=0;flag&&i<size-1;++i)  //i代表第幾次搜尋
+    {
+        flag=0; //flag==0代表已完成排序
+        for(int j=0;j<size-1-i;++j) //j代表比較第j對  (5,7) (7,6) (6,3) ...
+        {
+            if(store[j]>store[j+1]) //因為是從小到大,所以若前面的比後面的大就要交換
+            {
+                temp=store[j];
+                store[j]=store[j+1];
+                store[j+1]=temp;
+                flag=1;
+            }
+        }
+    }
 
-	int store[SIZE] = { 3,5,1,-7,4,9,-6,8,10,4 },temp,flag=1;
-
-	//small to big
-  
-	for (int i = 0; i < SIZE; ++i) //each round
-	{
-		for (int j = 0; j <SIZE-1-i; ++j)  //compare with neighbourhood 每一輪最大的會被移到最右邊
-		{
-			if (store[j] > store[j + 1])   //big to small: <
-			{
-				temp = store[j]; 
-				store[j] = store[j + 1];
-				store[j + 1] = temp;
-			}
-		}
-	}
-  
-	for (int i = 0; i < SIZE; ++i)
-		printf("%d ", store[i]);
-	printf("\n");
-  
-  
-	//improved ver.
-	for (int i = 0; !flag && i < SIZE; ++i) //each round   flag:該輪下來flag沒被更動,代表sorting已經完成(全部人都是由小到大排好了),可以提早離開了
-	{
-		flag = 1; //每一輪都要初始化,不然的話只要第一輪有動過,後面就必然無法提早離開(因為flag會維持0)
-		for (int j = 0; j < SIZE - 1 - i; ++j)  //compare with neighbourhood
-		{
-			if (store[j] > store[j + 1])   //big to small: <
-			{
-				temp = store[j];
-				store[j] = store[j + 1];
-				store[j + 1] = temp;
-				flag = 0;
-			}
-		}
-	}
-  
-	for (int i = 0; i < SIZE; ++i)
-		printf("%d ", store[i]);
-	printf("\n");
-
-	system("pause");
-	return 0;
+    for(int i=0;i<size;++i)
+        printf("%d ",store[i]);
+    printf("\n");
+    
+    
+    return 0;
 }
